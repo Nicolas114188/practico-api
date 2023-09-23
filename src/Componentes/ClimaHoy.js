2981550c3b7d7ac372ea1e954fd05cf54d69c405
Nicolas114188 <nicolasalberto.herrera@gmail.com>
2023-09-23 12:17:40 -0300

    modificacion con flex

--- src/Componentes/ClimaHoy.js
+++ src/Componentes/ClimaHoy.js
@@ -4,24 +4,24 @@ import duranteDia from "../Imagenes/Barra de clima del dia.jpg"
 
 function ClimaHoy(){
     return(
-        <body>
-            <h2>Hoy</h2>
-                <body className="actual">
-                    <img src="https://www.smn.gob.ar/sites/all/themes/smn/img/weather-icons/43.png" />
-                    <p>17°</p>
-                </body>
-                <body className="completo">
-                <body className="item-1">
-                    <h3>Mañana</h3>
-                    <img src="https://www.smn.gob.ar/sites/all/themes/smn/img/weather-icons/37.png"/>
-                    <p>Mayormente nublado</p>
-                </body>
-                <body className="item-2">
+        <body className="contenedor">
+                <div className="durante Dia">
                     <h3>Durante el dia</h3>
                     <img src={duranteDia} alt="durante Dia"/>
-                    <p>Mayormente nublado</p>
-                </body>
-                </body>
+                </div>
+                
+                <div className="completo">
+                    <div className="item-1">
+                        <h3>Mañana</h3>
+                        <img src="https://www.smn.gob.ar/sites/all/themes/smn/img/weather-icons/37.png"/>
+                        <p>Mayormente nublado</p>
+                    </div>
+                    <div className="aitem-2">
+                        <h3>Hoy</h3>
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
+                        <p>21°</p>
+                    </div>
+                </div>
         </body>
     );
 }
