8ad8a72640ca90e7075560a29afb430cd3473060
Nicolas114188 <nicolasalberto.herrera@gmail.com>
2023-10-03 19:06:43 -0300

    modificación en el clima

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import ClimaHoy from './Componentes/ClimaHoy';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <body className='contenedor'>
           <ClimaHoy/>
         </body>
-      </header>
     </div>
   );
 }
--- src/Componentes/ClimaHoy.js
+++ src/Componentes/ClimaHoy.js
@@ -1,50 +1,87 @@
 import React from "react";
-import "./climaHoy.css"
-import duranteDia from "../Imagenes/Barra de clima del dia.jpg"
+import "./climaHoy.css";
+import nublado from "../Imagenes/nublado.svg";
+import precipitacion from"../Imagenes/presimitaciones.svg"
+import styled from "styled-components";
+import { Bar } from "react-chartjs-2";
 
 function ClimaHoy(){
+    
     return(
-        <body className="contenedor">
-                <div className="durante Dia">
-                    <h3>Durante el dia</h3>
-                    <img src={duranteDia} alt="durante Dia"/>
-                </div>
-                
-                <div className="completo">
-                    <div className="item-1">
-                        <h3>Actualidad</h3>
-                        <img src="https://www.smn.gob.ar/sites/all/themes/smn/img/weather-icons/37.png"/>
+        <div className="contenedor">
+            <div className="item-1">
+                    <div className="Actualidad">
+                        Actualidad
+                        <img src={nublado} alt="nublado"/>
                         <p>Mayormente nublado</p>
                     </div>
-                    <div className="item-2">
-                        <h3>Hoy</h3>
-                        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" />
-                        <p>21°</p>
+                    <div className="hoy">
+                        Hoy
+                        <img src={precipitacion} alt="presipitacion" />
+                        <p>22°</p>
                     </div>
+            </div>
+            <div className="item-2">
+                    <div className="maxima">
+                        Maxima:
+                        22°
+                    </div>
+                    <div className="minima">
+                        Minima: 
+                        9°
+                    </div>
+            </div>
+            <div className="item-3">
+                Durante el día:
+                <div className="dia-horizontal">
+                        <p>40</p>
+                        <p>30</p>
+                        <p>20</p>
+                        <p>0</p>
                 </div>
-                <div className="servicio">
-                        <h3>Datos:</h3>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <div className="dia-vertical">
+                    <div className="barra b-1"></div>
+                    <div className="barra b-2"></div>
+                    <div className="barra b-3"></div>
+                    <div className="barra b-4"></div>
+                    <div className="barra b-5"></div>
+                    <div className="barra b-6"></div>
+                    <div className="barra b-7"></div>
+                    <div className="barra b-8"></div>
+                    <div className="barra b-9"></div>           
+                </div>
+                <br/>
+                <hr/>
+                <p>12:00 AM 3:00 AM 6:00 AM 9:00 AM 12:00 PM 3:00 PM 6:00 PM 9:00 PM 12:00AM</p>
+            </div>
+                <div className="item-4">
+                        Datos:
                         <br/>
-                        <div className="item ser-1">
+                        <div className="ser ser-1">
                             <p>indice UV</p>
                         </div>
-                        <div className="item ser-2">
+                        <div className="ser ser-2">
                             <p>Velocidad del viento</p>
                         </div>
-                        <div className="item ser-3">
+                        <div className="ser ser-3">
                             <p>Hora del sol</p>
                         </div>
-                        <div className="item ser-4">
+                        <div className="ser ser-4">
                             <p>Humedad</p>
                         </div>
-                        <div className="item ser-5">
+                        <div className="ser ser-5">
                             <p>Visibilidad</p>
                         </div>
-                        <div className="item ser-6">
+                        <div className="ser ser-6">
                             <p>Calidad del aire</p>
                         </div>
-                </div>
-        </body>
+                </div>             
+        </div>
     );
 }
 export default ClimaHoy;
