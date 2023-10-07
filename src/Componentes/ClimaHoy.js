a9d253771ee67cfcf008e9cc2ebdd6035544fabe
Nicolas114188 <nicolasalberto.herrera@gmail.com>
2023-10-07 20:26:57 -0300

    Readme y pagina finalisada

--- src/Componentes/ClimaHoy.js
+++ src/Componentes/ClimaHoy.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./climaHoy.css";
 import nublado from "../Imagenes/nublado.svg";
-import precipitacion from"../Imagenes/presimitaciones.svg"
-import styled from "styled-components";
-import { Bar } from "react-chartjs-2";
+import soliado from "../Imagenes/soliado.svg";
+import precipitacion from"../Imagenes/presimitaciones.svg";
+import datosClima from "./api.json";
 
 function ClimaHoy(){
-    
+    var tiempo=[];
+    for(let i=0;i<datosClima.hourly.time.length;i++)
+    {
+        tiempo[i]=datosClima.hourly.time[i];
+    };
+    var tempList=[];
+    for(let i=0;i<datosClima.hourly.temperature_2m.length;i++)
+    {
+        tempList[i]=datosClima.hourly.temperature_2m[i];
+    };  
+ 
     return(
         <div className="contenedor">
             <div className="item-1">
@@ -17,29 +27,32 @@ function ClimaHoy(){
                     </div>
                     <div className="hoy">
                         Hoy
-                        <img src={precipitacion} alt="presipitacion" />
-                        <p>22°</p>
+                        <img src={soliado} alt="soliado" />
+                        <p>{datosClima.current_weather.temperature} {datosClima.hourly_units.temperature_2m}</p>
+                    </div>
+                    <div className="dia">
+                        <p>{datosClima.daily.time}</p>
                     </div>
             </div>
             <div className="item-2">
                     <div className="maxima">
-                        Maxima:
-                        22°
+                       <p>Maxima:</p>
+                       <p>{datosClima.daily.temperature_2m_max} {datosClima.daily_units.temperature_2m_max}</p>
                     </div>
                     <div className="minima">
-                        Minima: 
-                        9°
+                        <p>Minima:</p> 
+                        <p>{datosClima.daily.temperature_2m_min} {datosClima.daily_units.temperature_2m_min}</p>      
                     </div>
             </div>
             <div className="item-3">
-                Durante el día:
+                <h3>Durante el día:</h3>
+                
                 <div className="dia-horizontal">
-                        <p>40</p>
-                        <p>30</p>
-                        <p>20</p>
+                        <p>{datosClima.hourly.temperature_2m[8]}</p>
+                        <p>{datosClima.hourly.temperature_2m[4]}</p>
+                        <p>{datosClima.hourly.temperature_2m[23]}</p>
                         <p>0</p>
                 </div>
-                <br/>
                 <br/>
                 <br/>
                 <br/>
@@ -57,28 +70,43 @@ function ClimaHoy(){
                 </div>
                 <br/>
                 <hr/>
-                <p>12:00 AM 3:00 AM 6:00 AM 9:00 AM 12:00 PM 3:00 PM 6:00 PM 9:00 PM 12:00AM</p>
+                <p className="paramDia">{datosClima.hourly.time[0]} 
+                                        {datosClima.hourly.time[3]}
+                                        {datosClima.hourly.time[6]}
+                                        {datosClima.hourly.time[9]}
+                                        {datosClima.hourly.time[12]}
+                                        {datosClima.hourly.time[15]}
+                                        {datosClima.hourly.time[18]}
+                                        {datosClima.hourly.time[21]}
+                                        {datosClima.hourly.time[23]}</p>
             </div>
                 <div className="item-4">
                         Datos:
                         <br/>
                         <div className="ser ser-1">
                             <p>indice UV</p>
+                            <p>{datosClima.daily.uv_index_max}</p>
                         </div>
                         <div className="ser ser-2">
                             <p>Velocidad del viento</p>
+                            <p>{datosClima.daily.windspeed_10m_max} {datosClima.daily_units.windspeed_10m_max}</p>
                         </div>
                         <div className="ser ser-3">
                             <p>Hora del sol</p>
+                            <p>Amanacer: {datosClima.daily.sunrise}</p>
+                            <p>Atardecer: {datosClima.daily.sunset}</p>
                         </div>
                         <div className="ser ser-4">
                             <p>Humedad</p>
+                            <p>{datosClima.hourly.relativehumidity_2m[0]} {datosClima.hourly_units.relativehumidity_2m}</p>
                         </div>
                         <div className="ser ser-5">
                             <p>Visibilidad</p>
+                            <p>{datosClima.hourly.visibility[0]} {datosClima.hourly_units.visibility}</p>
                         </div>
                         <div className="ser ser-6">
                             <p>Calidad del aire</p>
+                            <p>{datosClima.hourly.european_aqi[18]} </p>
                         </div>
                 </div>             
         </div>
