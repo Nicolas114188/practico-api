2facd85114362e2f2456ae08fec5fed76362077e
Nicolas114188 <nicolasalberto.herrera@gmail.com>
2023-10-13 07:30:12 -0300

    Datos tomado desde la Api clima

--- src/Componentes/ClimaHoy.js
+++ src/Componentes/ClimaHoy.js
@@ -4,53 +4,73 @@ import nublado from "../Imagenes/nublado.svg";
 import soliado from "../Imagenes/soliado.svg";
 import precipitacion from"../Imagenes/presimitaciones.svg";
 import datosClima from "./api.json";
-
+import {useState, useEffect} from "react";
+//https://api.open-meteo.com/v1/forecast?latitude=-31.4135&longitude=-64.181&current=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m,winddirection_10m&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,precipitation,visibility&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=America%2FSao_Paulo
 function ClimaHoy(){
-    var tiempo=[];
+    const [datostiempo, setdatostiempo] =useState(null);
+    const[cargando,setcargardo]=useState(false);
+    /*var tiempo=[];
     for(let i=0;i<datosClima.hourly.time.length;i++)
     {
         tiempo[i]=datosClima.hourly.time[i];
     };
-    var tempList=[];
-    for(let i=0;i<datosClima.hourly.temperature_2m.length;i++)
+    var tempList=[];*/
+    /*for(let i=0;i<datosClima.hourly.temperature_2m.length;i++)
     {
         tempList[i]=datosClima.hourly.temperature_2m[i];
-    };  
- 
+    };  */
+    useEffect(()=>{
+        fetch('https://api.open-meteo.com/v1/forecast?latitude=-31.4135&longitude=-64.181&current=temperature_2m,relativehumidity_2m,weathercode,windspeed_10m,winddirection_10m&hourly=temperature_2m,relativehumidity_2m,precipitation_probability,precipitation,visibility&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max&timezone=America%2FSao_Paulo')
+        .then(resp=>resp.json()).then(data=>{
+            setdatostiempo(data);
+            //console.log(data);
+        }).catch(ex =>{console.error(ex);})
+    },[])
+    //console.log(datostiempo);
+    if(datostiempo==null)
+    {
+        return(
+            <div>
+                <h1>Cargando datos...</h1>
+            </div>
+        );
+    }else
+    {
+
     return(
         <div className="contenedor">
             <div className="item-1">
                     <div className="Actualidad">
                         Actualidad
-                        <img src={nublado} alt="nublado"/>
-                        <p>Mayormente nublado</p>
+                        <img src={soliado} alt="soliado"/>
+                        <p>Soleado</p>
                     </div>
                     <div className="hoy">
                         Hoy
                         <img src={soliado} alt="soliado" />
-                        <p>{datosClima.current_weather.temperature} {datosClima.hourly_units.temperature_2m}</p>
+                        <p>{datostiempo.current.temperature_2m} {datostiempo.hourly_units.temperature_2m}</p>
                     </div>
                     <div className="dia">
-                        <p>{datosClima.daily.time}</p>
+                        <p>{datostiempo.daily.time[0]}</p>
                     </div>
             </div>
             <div className="item-2">
                     <div className="maxima">
                        <p>Maxima:</p>
-                       <p>{datosClima.daily.temperature_2m_max} {datosClima.daily_units.temperature_2m_max}</p>
+                       <p>{datostiempo.daily.temperature_2m_max[0]} {datostiempo.daily_units.temperature_2m_max}</p>
                     </div>
                     <div className="minima">
                         <p>Minima:</p> 
-                        <p>{datosClima.daily.temperature_2m_min} {datosClima.daily_units.temperature_2m_min}</p>      
+                        <p>{datostiempo.daily.temperature_2m_min[0]} {datostiempo.daily_units.temperature_2m_min}</p>      
                     </div>
             </div>
             <div className="item-3">
                 <h3>Durante el día:</h3>
                 
                 <div className="dia-horizontal">
-                        <p>{datosClima.hourly.temperature_2m[8]}</p>
-                        <p>{datosClima.hourly.temperature_2m[4]}</p>
-                        <p>{datosClima.hourly.temperature_2m[23]}</p>
+                        <p>{datostiempo.hourly.temperature_2m[16]}</p>
+                        <p>{datostiempo.hourly.temperature_2m[11]}</p>
+                        <p>{datostiempo.hourly.temperature_2m[7]}</p>
                         <p>0</p>
                 </div>
                 <br/>
@@ -70,39 +90,39 @@ function ClimaHoy(){
                 </div>
                 <br/>
                 <hr/>
-                <p className="paramDia">{datosClima.hourly.time[0]} 
-                                        {datosClima.hourly.time[3]}
-                                        {datosClima.hourly.time[6]}
-                                        {datosClima.hourly.time[9]}
-                                        {datosClima.hourly.time[12]}
-                                        {datosClima.hourly.time[15]}
-                                        {datosClima.hourly.time[18]}
-                                        {datosClima.hourly.time[21]}
-                                        {datosClima.hourly.time[23]}</p>
+                <p className="paramDia">{datostiempo.hourly.time[0]} 
+                                        {datostiempo.hourly.time[3]}
+                                        {datostiempo.hourly.time[6]}
+                                        {datostiempo.hourly.time[9]}
+                                        {datostiempo.hourly.time[12]}
+                                        {datostiempo.hourly.time[15]}
+                                        {datostiempo.hourly.time[18]}
+                                        {datostiempo.hourly.time[21]}
+                                        {datostiempo.hourly.time[23]}</p>
             </div>
                 <div className="item-4">
                         Datos:
                         <br/>
                         <div className="ser ser-1">
                             <p>indice UV</p>
-                            <p>{datosClima.daily.uv_index_max}</p>
+                            <p>{datostiempo.daily.uv_index_max[0]}</p>
                         </div>
                         <div className="ser ser-2">
                             <p>Velocidad del viento</p>
-                            <p>{datosClima.daily.windspeed_10m_max} {datosClima.daily_units.windspeed_10m_max}</p>
+                            <p>{datostiempo.current.windspeed_10m} {datostiempo.current_units.windspeed_10m}</p>
                         </div>
                         <div className="ser ser-3">
                             <p>Hora del sol</p>
-                            <p>Amanacer: {datosClima.daily.sunrise}</p>
-                            <p>Atardecer: {datosClima.daily.sunset}</p>
+                            <p>Amanacer: {datostiempo.daily.sunrise[0]}</p>
+                            <p>Atardecer: {datostiempo.daily.sunset[0]}</p>
                         </div>
                         <div className="ser ser-4">
                             <p>Humedad</p>
-                            <p>{datosClima.hourly.relativehumidity_2m[0]} {datosClima.hourly_units.relativehumidity_2m}</p>
+                            <p>{datostiempo.current.relativehumidity_2m} {datostiempo.current_units.relativehumidity_2m}</p>
                         </div>
                         <div className="ser ser-5">
                             <p>Visibilidad</p>
-                            <p>{datosClima.hourly.visibility[0]} {datosClima.hourly_units.visibility}</p>
+                            <p>{datostiempo.hourly.visibility[0]} {datostiempo.hourly_units.visibility}</p>
                         </div>
                         <div className="ser ser-6">
                             <p>Calidad del aire</p>
@@ -111,5 +131,6 @@ function ClimaHoy(){
                 </div>             
         </div>
     );
+    }
 }
 export default ClimaHoy;
